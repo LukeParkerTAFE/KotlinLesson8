4219f3ae9fe91cba626e59008e9bcd0f20141ebc
Luke Parker <luke.parker9@tafensw.edu.au>
2021-07-11 19:17:24 +1000

    End of Lesson 8-05

--- src/main/kotlin/com/lukeparker/videodemo/controllers/PersonController.kt
+++ src/main/kotlin/com/lukeparker/videodemo/controllers/PersonController.kt
@@ -1,30 +1,40 @@
 package com.lukeparker.videodemo.controllers
 
 import com.lukeparker.videodemo.models.Person
+import com.lukeparker.videodemo.models.PersonCreateModel
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("persons")
 class PersonController {
 
     @GetMapping
-    fun getAllPeople() : ResponseEntity<List<Person>> {
+    fun getAllPeople(): ResponseEntity<List<Person>> {
         return ResponseEntity.ok(people)
     }
 
     @GetMapping("{id}")
     fun getPersonById(@PathVariable id: Int): ResponseEntity<Person> {
-        val person = people.find{p -> p.id == id}
+        val person = people.find { p -> p.id == id }
         if (person == null) {
             // Return 404
             return ResponseEntity.notFound().build()
         } else {
             return ResponseEntity.ok(person)
         }
+    }
+
+    @PostMapping
+    fun createPerson(createModel: PersonCreateModel): Person {
+        val person = Person(
+            (1..1_000_000).random(),
+            createModel.firstName,
+            createModel.lastName,
+            createModel.age,
+        )
+        people.add(person)
+        return person
     }
 
     companion object {
---
+++ src/main/kotlin/com/lukeparker/videodemo/models/PersonCreateModel.kt
@@ -0,0 +1,7 @@
+package com.lukeparker.videodemo.models
+
+data class PersonCreateModel(
+    var firstName: String,
+    var lastName: String,
+    var age: Int,
+)

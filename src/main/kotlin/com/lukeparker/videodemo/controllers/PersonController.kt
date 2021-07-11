95b6e3e55795c33c2ce0c496a68e5e200779882c
Luke Parker <luke.parker9@tafensw.edu.au>
2021-07-11 18:59:25 +1000

    End of Lesson 8-04

--- src/main/kotlin/com/lukeparker/videodemo/controllers/PersonController.kt
+++ src/main/kotlin/com/lukeparker/videodemo/controllers/PersonController.kt
@@ -3,6 +3,7 @@ package com.lukeparker.videodemo.controllers
 import com.lukeparker.videodemo.models.Person
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,6 +14,17 @@ class PersonController {
     @GetMapping
     fun getAllPeople() : ResponseEntity<List<Person>> {
         return ResponseEntity.ok(people)
+    }
+
+    @GetMapping("{id}")
+    fun getPersonById(@PathVariable id: Int): ResponseEntity<Person> {
+        val person = people.find{p -> p.id == id}
+        if (person == null) {
+            // Return 404
+            return ResponseEntity.notFound().build()
+        } else {
+            return ResponseEntity.ok(person)
+        }
     }
 
     companion object {

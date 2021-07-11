be821625014ccc0cdeb7e85975a839c76adb52be
Luke Parker <luke.parker9@tafensw.edu.au>
2021-07-11 18:23:11 +1000

    End of Lesson 8-03

--- src/main/kotlin/com/lukeparker/videodemo/controllers/PersonController.kt
+++ src/main/kotlin/com/lukeparker/videodemo/controllers/PersonController.kt
@@ -1,12 +1,20 @@
 package com.lukeparker.videodemo.controllers
 
 import com.lukeparker.videodemo.models.Person
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("persons")
 class PersonController {
+
+    @GetMapping
+    fun getAllPeople() : ResponseEntity<List<Person>> {
+        return ResponseEntity.ok(people)
+    }
+
     companion object {
         var people = mutableListOf(
             Person(
